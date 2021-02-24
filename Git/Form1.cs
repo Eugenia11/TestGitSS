d91eeb82d75f3541f27542b6107fdbae28706b2d
Eugenia11 <54139665+Eugenia11@users.noreply.github.com>
2021-02-24 13:24:45 +0200

    Update Form1.cs

--- Git/Form1.cs
+++ Git/Form1.cs
@@ -19,7 +19,7 @@ namespace Git
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello!");
+            MessageBox.Show("Hello world!");
         }
     }
 }
